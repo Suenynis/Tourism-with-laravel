36f6878a5008c0979bd1afdd9f4ba05b8fad4819
Sueny <mirambekov.sueny@gmail.com>
2022-05-31 23:02:13 +0600

    little changes

--- app/Http/Controllers/User_for_Admin_Controller.php
+++ app/Http/Controllers/User_for_Admin_Controller.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
 class User_for_Admin_Controller extends Controller
 {
     public function show(){
